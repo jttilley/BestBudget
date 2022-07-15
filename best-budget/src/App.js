7ce5acf36c779591f74d79f69ba65456aa838a45
Jason Tilley <jttilley007@yahoo.com>
2022-07-15 01:04:53 -0600

    Got an accordian working

--- best-budget/src/App.js
+++ best-budget/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import AddBudgetItem from './components/AddBudgetItem';
+import CssBaseline from '@mui/material/CssBaseline';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline/>
+      <Categories/>
       <AddBudgetItem/>
     </div>
   );
--- best-budget/src/components/AddBudgetItem.js
+++ best-budget/src/components/AddBudgetItem.js
@@ -3,32 +3,44 @@ import TextField from '@mui/material/TextField';
 // import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Autocomplete from '@mui/material/Autocomplete';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+// import { styled } from '@mui/material/styles';
+// import Paper from '@mui/material/Paper';
 import { personalCategories } from '../utils/allCategories';
+import { Button } from '@mui/material';
 
-const itemStyling = styled(Paper)(({ theme }) => ({
-  paddingLeft: theme.spacing(5),
-}));
 
-export default function AddBudgetItem() {
+// const itemStyling = styled(Paper)(({ theme }) => ({
+//   paddingLeft: theme.spacing(5),
+// }));
+
+export default function AddBudgetItem({ category }) {
+  const itemId = `${category}_itemDesc`;
+  const amountId = `${category}_amount`;
+
   return (
     <div>
-      <Grid container>
-        <Grid itemStyling xs={4}>
+      <Grid container sx={{ padding: 2 }}>
+        <Grid itemStyling xs={5}>
           <Autocomplete
             // disablePortal
-            id="itemDesc"
+            freeSolo
+            id={itemId}
             options={personalCategories[2].subCategories.map((option) => option)}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} variant="standard" label="Budget Item" />}
+            
+            renderInput={(params) => <TextField {...params} variant="outlined" label="Budget Item" />}
           />
         </Grid>
-        <Grid xs = {2}>
-          <TextField sx={{ width: 150 }} id="amount" label="Amount" variant="standard" />
+        <Grid xs={4}>
+          <TextField id={amountId} label="Amount" variant="outlined" />
         </Grid>
-
+        <Grid xs={3}>
+          <Button variant="contained" color="success" sx={{ display:"flex", alignSelf: "flex-end", width: 100 }} onClick={AddItem}>Add</Button>
+        </Grid>
       </Grid>
     </div>
   );
+}
+
+const AddItem = () => {
+  alert("I'm adding the item");
 }
